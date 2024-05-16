6cd22b4fcc8db3b6f1940bcbc8c2bea1b7d97a23
Kirill Meng <kirill.meng@wingcon.com>
2024-05-16 11:06:07 +0200

    add package info

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule DatemathEx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
     ]
   end
 
@@ -15,6 +17,21 @@ defmodule DatemathEx.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp description do
+    """
+    A parser for datemath syntax.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: "wingcon",
+      licenses: ~w(MIT),
+      links: %{"Github" => "https://github.com/wingcon/datemath_ex"}
     ]
   end
 
